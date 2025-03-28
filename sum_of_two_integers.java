d549a2c9ee62abbbc892e252572fad8a9be6aac9
Surya3699 <41103479+Surya3699@users.noreply.github.com>
2025-03-28 13:08:05 +0530

    Create sum_of_two_integers.java

---
+++ sum_of_two_integers.java
@@ -0,0 +1,11 @@
+# 1. Sum of two integers
+import java.util.*;
+
+class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    int sum = num1 + num2;
+    System.out.println(sum);
+}
