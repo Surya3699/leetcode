8ce42af2fec60538d5e76e57f66b6a136ea5ebb4
Surya3699 <41103479+Surya3699@users.noreply.github.com>
2025-03-28 16:18:31 +0530

    Create root_equals_sum_of_children.java

---
+++ root_equals_sum_of_children.java
@@ -0,0 +1,24 @@
+# 2 TreeNode check if sum of left and right node is equal to the root node ( 3 node tree)
+   
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode() {
+    
+  }
+  TreeNode(int val) { 
+    this.val = val; 
+  }
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        return (root.val == root.left.val + root.right.val);
+    }
+}
